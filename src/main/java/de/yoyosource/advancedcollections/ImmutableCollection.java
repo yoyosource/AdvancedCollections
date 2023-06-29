fa16543f92b85f8a5c6b3e27824312f0abea0795
yoyosource <yoyosource@nidido.de>
2023-06-29 16:34:44 +0200

    Initial commit

--- src/main/java/de/yoyosource/advancedcollections/ImmutableCollection.java
+++ src/main/java/de/yoyosource/advancedcollections/ImmutableCollection.java
@@ -1,4 +1,34 @@
 package de.yoyosource.advancedcollections;
 
-public interface MutableCollection {
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+
+public interface ImmutableCollection<T> extends Iterable<T> {
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean contains(T element);
+
+    Iterator<T> iterator();
+
+    T[] toArray();
+
+    boolean containsAll(Iterable<T> elements);
+
+    boolean equals(Object object);
+
+    int hashCode();
+
+    @Override
+    default Spliterator<T> spliterator() {
+        return Spliterators.spliterator(iterator(), size(), 0);
+    }
+
+    // Custom methods
+    default T any() {
+        return iterator().next();
+    }
 }
--- src/main/java/de/yoyosource/advancedcollections/MutableCollection.java
+++ src/main/java/de/yoyosource/advancedcollections/MutableCollection.java
@@ -1,2 +1,20 @@
-package de.yoyosource.advancedcollections;public interface MutableCollection {
+package de.yoyosource.advancedcollections;
+
+import java.util.function.Predicate;
+
+public interface MutableCollection<T> extends ImmutableCollection<T> {
+
+    boolean add(T element);
+
+    boolean remove(T element);
+
+    boolean addAll(Iterable<T> elements);
+
+    boolean removeAll(Iterable<T> elements);
+
+    boolean removeIf(Predicate<? super T> predicate);
+
+    boolean retainAll(Iterable<T> elements);
+
+    void clear();
 }
